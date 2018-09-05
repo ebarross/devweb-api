05b16d7ddb85cad6db92ef3b9d3ac3e27c2e9d23
Emanoel Barros <ebarros297@gmail.com>
2018-09-05 01:20:32 -0300

    adiciona bodyParser e morgan

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
+const app = express();
 const port = 8080;
 
 const restaurante = require('./routes/restaurante');
@@ -8,13 +10,20 @@ const restaurante = require('./routes/restaurante');
 app.listen(port);
 console.log(`Servidor rodando na porta ${port}...`);
 
+app.use(morgan('combined'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.header('Content-type', 'application/json');
     next();
 });
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
     res.send(JSON.stringify('Receiving GET request to /'));
+});
+
+app.post('/', (req, res) => {
+    res.end(JSON.stringify(req.body, null, 2));
 });
 
 app.use('/restaurante', restaurante);
